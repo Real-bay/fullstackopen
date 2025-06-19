ae20172ab840f3ae5578abb57483f7c149017344
Matias Aitolahti <96495033+Real-bay@users.noreply.github.com>
2025-06-19 12:20:41 +0300

    Part1 1.1

---
+++ part1/src/App.jsx
@@ -0,0 +1,45 @@
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ parts, exercises }) => {
+  return (
+    <>
+      <p>
+        {parts.part1} {exercises.exercises1}
+      </p>
+      <p>
+        {parts.part2} {exercises.exercises2}
+      </p>
+      <p>
+        {parts.part3} {exercises.exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = ({ exercises }) => {
+  return <p>Number of exercises {exercises.exercises1 + exercises.exercises2 +exercises.exercises3}</p>
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={{part1, part2, part3}} exercises={{exercises1, exercises2, exercises3}}/>
+      <Total exercises={{exercises1, exercises2, exercises3}}/>
+    </div>
+  )
+}
+
+export default App
